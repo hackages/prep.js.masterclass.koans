ed0d4fff6cd4d5ec56a19f0fed28fdf2655b1099
Antonio Calapez <antonio@hackages.io>
2017-06-01 15:25:50 +0200

    Adding tests for JS functional pattern

--- __tests__/advanced.js
+++ __tests__/advanced.js
@@ -57,3 +57,48 @@ describe("About functions", () => {
     expect(returnAllArgs("first", "second", "third")).to.equal(__);
   });
 });
+
+describe("JavaScript Inheritance Functional Patterns", () => {
+  const animal = function(name) {
+    const that = {};
+    that.name = name || "";
+    that.gender = "";
+    that.planetOfBirth = "Earth";
+    that.getGender = function() {
+      return this.gender;
+    };
+    return that;
+  };
+
+  const male = function(name) {
+    const that = animal(name);
+    that.name = name;
+    that.gender = "Male";
+    return that;
+  };
+
+  const dog = male("joe");
+  it("Analysing dog", () => {
+    // What will be his gender?
+    // Where was he born?
+  });
+
+  const female = function(name) {
+    const that = animal(name);
+    that.name = name;
+    that.gender = "Female";
+    return that;
+  };
+
+  const cat = female("ana");
+  it("Analysing cat", () => {
+    // What will be his gender?
+    // Where was he born?
+  });
+
+  it("Analysing dog and cat", () => {
+    // Changing the planetOfBirth for the dog to "Pluton"
+    dog.planetOfBirth = "Pluton";
+    // Where were they born?
+  });
+});
