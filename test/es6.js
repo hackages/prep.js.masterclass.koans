ffc717f8eeb0e16e0bed3b123f32fdc6bd960c07
Milenko Vorkapic <milenko@hackages.io>
2017-10-09 16:48:04 +0200

    Destructuring

--- test/es6.js
+++ test/es6.js
@@ -1,61 +1,16 @@
 import { expect, should } from 'chai';
 
 describe('ES6 ', () => {
-  describe('var, let, const', () => {
-    describe('var...', () => {
-      function iterator(bool) {
-        if (bool) {
-          var name = 'Hackages';
-        }
-        return name;
-      }
 
-      it('what do you expect???', () => {
-        const actual = iterator(false);
-
-        expect(actual).equal(__);
-      });
-    });
-
-    xdescribe('let...', () => {
-      function iterator(bool) {
-        if (bool) {
-          let name = 'Hackages';
-        }
-        return name;
-      }
-
-      it('what do you expect???', () => {
-        // TODO: uncomment this line below to see what happens when you change var to let
-        //console.log(iterator());
-      });
-    });
-
-    xdescribe('const...', () => {
-      function iterator(bool) {
-        if (bool) {
-          const name = 'we';
-        }
-        return name;
-      }
-
-      it('what do you expect???', () => {
-        // TODO: uncomment this line below to see what happens when you change var to const
-        //console.log(iterator());
-      });
-    });
-  });
-
-  // You can implement your solution in another file or inline here
-  // You can find more information about arrow-function mdn by following the link below
-  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+    // You can implement your solution in another file or inline here
+    // You can find more information about arrow-function mdn by following the link below
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 
   describe('Arrow function', () => {
     describe('Invoked through call or apply', () => {
       it('Should not bind this through call', () => {
         const person = {
           age: 1,
-
           add: function(age) {
             const f = v => v + this.age;
             return f(age);
@@ -110,123 +65,151 @@ describe('ES6 ', () => {
           },
         };
 
-        Object.defineProperty(person, 'getAgeProperty', {
-          get: () => {
-            return this ? this.age : undefined;
-          },
+          Object.defineProperty(person, 'getAgeProperty', {
+            get: () => {
+              return this ? this.age : undefined;
+            },
+          });
+
+          let actual = person.getAge();
+
+          expect(actual).equal(__);
+
+          actual = person.getAgeByArrow();
+
+          expect(actual).equal(__);
+
+          actual = person.getAgeProperty;
         });
-
-        let actual = person.getAge();
-
-        expect(actual).equal(__);
-
-        actual = person.getAgeByArrow();
-
-        expect(actual).equal(__);
-
-        actual = person.getAgeProperty;
       });
     });
-  });
 
-  describe('Class in ES6', () => {
-    describe('Like a function ...', () => {
-      // Declare the Human class below that will satisfy all assertions
-      let Human;
-      it('should be a function', () => {
-        expect(Human).to.be.a('function');
+    describe('Class in ES6', () => {
+      describe('Like a function ...', () => {
+        // Declare the Human class below that will satisfy all assertions
+        let Human;
+        it('should be a function', () => {
+          expect(Human).to.be.a('function');
+        });
+        it('should not be hoisted like function', () => {
+          let polyFunc;
+
+          (() => {
+            polyFunc = new PolygoneFunc(3, 5);
+          }).should.not.throw(Error);
+
+          function PolygoneFunc(height, width) {
+            this.height = height;
+            this.width = width;
+
+            this.log = function() {
+              return `H:${this.height} & W:${this.width}`;
+            };
+          }
+
+          let actual = polyFunc.log();
+
+          expect(actual).equal('H:3 & W:5');
+
+          let polyClass;
+
+          // Hint: the declaration position of PolygoneClass is not correct
+          (() => {
+            polyClass = new PolygoneClass(3, 5);
+          }).should.not.throw(Error);
+
+          class PolygoneClass {
+            constructor(height, width) {
+              this.height = height;
+              this.width = width;
+            }
+
+            log() {
+              return `H:${this.height} & W:${this.width}`;
+            }
+          }
+          actual = polyClass.log();
+
+          expect(actual).equal('H:3 & W:5');
+        });
       });
-      it('should not be hoisted like function', () => {
-        let polyFunc;
+    });
 
-        (() => {
-          polyFunc = new PolygoneFunc(3, 5);
-        }).should.not.throw(Error);
-
-        function PolygoneFunc(height, width) {
-          this.height = height;
-          this.width = width;
-
-          this.log = function() {
-            return `H:${this.height} & W:${this.width}`;
+    describe('Destructuring', () => {
+      describe('Object', () => {
+        function getUserInfo() {
+          return {
+            id: 8798,
+            name: 'Davy Engone',
+            company: 'Hackages',
+            country: 'Everywhere',
+            handles: {
+              twitter: 'davyengone',
+              skype: 'davy_engone'
+            }
           };
         }
 
-        let actual = polyFunc.log();
+        it('With ES6 object Destructuring to do the same operation', () => {
+          // TODO Extract the required information using the spread operator.
+          expect(id).to.be.defined;
+          expect(fullName).to.equal('Davy Engone');
+          expect(twitter).to.equal('davyengone');
+        });
+      })
 
-        expect(actual).equal('H:3 & W:5');
+      describe('Array', ()=>{
+        let fruits = ['brussels sprout', 'apple', 'beetroot', 'broccoli', 'carrot', 'cherry'];
 
-        let polyClass;
+        it('With ES5', () => {
+          const first = fruits[0];
+          expect(first).to.equal('brussels sprout');
+          const last = fruits[fruits.length - 1];
+          expect(last).to.equal('cherry');
+        });
 
-        // Hint: the declaration position of PolygoneClass is not correct
-        (() => {
-          polyClass = new PolygoneClass(3, 5);
-        }).should.not.throw(Error);
+        it('With ES6', () => {
+          // TODO: Get the first fruit
+          let actual;
+          expect(actual).to.equal('brussels sprout');
+          // TODO: Get the last fruit
+          expect(actual).to.equal('cherry');
+          // TODO: Get the queue fruit;
+          expect(actual).deep.equal(['apple', 'beetroot', 'broccoli', 'carrot', 'cherry']);
+        });
+      });
+    })
 
-        class PolygoneClass {
-          constructor(height, width) {
-            this.height = height;
-            this.width = width;
-          }
+    describe('Default parameters', () => {
+      it("With ES5", () => {
 
-          log() {
-            return `H:${this.height} & W:${this.width}`;
-          }
+        function drawES5Chart(options) {
+          options = options === undefined ? {} : options;
+          const size = options.size === undefined ? 'big' : options.size;
+          const cords = options.cords === undefined ? { x: 0, y: 0 } : options.cords;
+          const radius = options.radius === undefined ? 25 : options.radius;
+          return [size, cords, radius];
         }
+        const actual = drawES5Chart({
+          cords: { x: 18, y: 30 },
+          radius: 30
+        });
+        expect(actual).deep.equal(['big', { x: 18, y: 30 }, 30]);
 
-        actual = polyClass.log();
+        });
+      it("With ES6", () => {
 
-        expect(actual).equal('H:3 & W:5');
+        function drawES6Chart(/* // TODO: Provide the parameter desctructuring to satisfy all assertions below  */) {
+          return [size, cords, radius];
+        }
+        let actual = drawES6Chart();
+        expect(actual).deep.equal(['big', { x: 0, y: 0 }, 25]);
+
+        actual = drawES6Chart({
+          cords: { x: 18, y: 30 },
+          radius: 30
+        });
+        expect(actual).deep.equal(['big', { x: 18, y: 30 }, 30]);
       });
     });
   });
-
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
-  // http://koajs.com/
-  // http://www.2ality.com/2015/03/es6-generators.html
-
-  describe('Generators', () => {
-    function getGeneratorResultInArray(func) {
-      const res = [];
-      for (let val of func()) {
-        res.push(val);
-      }
-      return res;
-    }
-
-    describe('Syntax', () => {
-      it('Via function declaration', () => {
-        // Declare and Implement the generator function foo that will satisfy all assertions
-
-        const actual = getGeneratorResultInArray(foo);
-        expect(actual).deep.equal(['a', 1, 'c']);
-      });
-
-      it('Via function expression', () => {
-        // Declare and Implement the generator function foo that will satisfy all assertions
-        let foo;
-
-        const actual = getGeneratorResultInArray(foo);
-        expect(actual).deep.equal(['d', 1, 'lol']);
-      });
-
-      it('Via a method definition in an object literal', () => {
-        let obj = {
-          // Declare and Implement the generator function foo that will satisfy all assertions
-        };
-
-        const actual = getGeneratorResultInArray(obj.foo);
-        expect(actual).deep.equal(['daddy', 1000, -100]);
-      });
-
-      it('Via a method definition in a class definition', () => {
-        class Bar {
-          // Declare and Implement the generator function foo that will satisfy all assertions
-        }
-
-        const actual = getGeneratorResultInArray(new Bar().foo);
-        expect(actual).deep.equal(['Mu', 1000, { name: 'Hackages' }]);
-      });
-    });
-  });
-});
